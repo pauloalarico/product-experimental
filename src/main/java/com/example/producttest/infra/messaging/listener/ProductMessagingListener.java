6fa6feabb478297ac76f98200ad021c1c04e4452
pauloalarico <paulocraft20@gmail.com>
2026-06-03 02:26:10 -0300

    feat: add listener that's will be listening at order created event

--- src/main/java/com/example/producttest/infra/messaging/listener/ProductMessagingListener.java
+++ src/main/java/com/example/producttest/infra/messaging/listener/ProductMessagingListener.java
@@ -1,4 +1,31 @@
 package com.example.producttest.infra.messaging.listener;
 
+import com.example.producttest.application.handler.OrderEventHandler;
+import com.example.producttest.infra.messaging.event.OrderCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class ProductMessagingListener {
+
+    private final OrderEventHandler handler;
+
+    public ProductMessagingListener(OrderEventHandler handler) {
+        this.handler = handler;
+    }
+
+    @KafkaListener(topics = "${app.kafka.listener.order-topic}")
+    public void onOrderCreated(@Payload OrderCreatedEvent event, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                               Acknowledgment ack) {
+        log.info("Received event on topic {}, correlationId {}", topic, event.correlationId());
+        handler.handle(event);
+        ack.acknowledge();
+    }
+
 }
