a580a10548083619ef510ec0d671c363c70ddcbb
pauloalarico <paulocraft20@gmail.com>
2026-06-06 04:16:03 -0300

    refactor: adapt use case for call the product publisher

--- src/main/java/com/example/producttest/application/usecase/VerifyStockUseCase.java
+++ src/main/java/com/example/producttest/application/usecase/VerifyStockUseCase.java
@@ -4,12 +4,13 @@ import com.example.producttest.domain.command.ConsultProductCommand;
 import com.example.producttest.domain.event.ReservedItem;
 import com.example.producttest.domain.event.StockReserved;
 import com.example.producttest.domain.port.out.ProductPersist;
+import com.example.producttest.domain.port.out.ProductPublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
@@ -17,8 +18,11 @@ public class VerifyStockUseCase {
 
     private final ProductPersist repository;
 
-    public VerifyStockUseCase(ProductPersist repository) {
+    private final ProductPublisher publisher;
+
+    public VerifyStockUseCase(ProductPersist repository, ProductPublisher publisher) {
         this.repository = repository;
+        this.publisher = publisher;
     }
 
     public Optional<List<ReservedItem>> verify(ConsultProductCommand command) {
@@ -31,16 +35,16 @@ public class VerifyStockUseCase {
             var product = repository.findById(id);
             if (product.getStockQuantity() < quantity) {
                 log.error("Stock not greater than order for product {}. A total of {} in stock, requested {}", product.getName(), product.getStockQuantity(), quantity);
-                return Optional.empty();
+                publisher.publish(new StockReserved(command.correlationId(), command.orderId(), null,
+                        StockReserved.StockStatus.OUT_OF_STOCK));
             }
-
             product.reduceStock(quantity);
             log.info("Reduce stock of product {}, reduced to {}. With a total of {} in stock", product.getName(), quantity, product.getStockQuantity());
             reservedItems.add(new ReservedItem(product.getId(), product.getName(), product.getType(),
                     product.getPrice(), quantity));
-
         }
-        return Optional.of(reservedItems);
+        publisher.publish(new StockReserved(command.correlationId(), command.orderId(),
+                reservedItems, StockReserved.StockStatus.RESERVED));
     }
 
 }
